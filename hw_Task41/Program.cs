6d4056a10e5c298bb2b09af49cae730c142acd30
Марина Донцова <dontsovam@mail.ru>
2022-10-07 17:59:21 +0300

    Решила задачу №41 - счетчик положительных чисел

---
+++ hw_Task41/Program.cs
@@ -0,0 +1,26 @@
+﻿//      Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+//      сколько чисел больше 0 ввёл пользователь.
+//      Ввод чисел останавливается при помощи ввода стоп-слова "stop"
+//      0, 7, 8, -2, -2 -> 2   /   1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите последовательно числа:  ");
+
+int countOfPositive = 0;
+
+while (true)
+{
+    string answer = Console.ReadLine();
+    if (answer == "stop")
+    {
+        break;
+    }
+    else
+    {
+        int number = Convert.ToInt32(answer);
+        if (number > 0)
+        {
+            countOfPositive++;
+        }
+    }
+}
+Console.WriteLine($"Число положительных чисел: {countOfPositive}");
